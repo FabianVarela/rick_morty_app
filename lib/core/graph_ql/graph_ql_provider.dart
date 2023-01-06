4aec1d6e1c6c16bacc3dc2dd223f3b1a3364e6f5
FabianVarela <jfabian.303@gmail.com>
2023-01-05 22:47:36 -0500

    Create provider to initialize and inject graphQL client

---
+++ lib/core/graph_ql/graph_ql_provider.dart
@@ -0,0 +1,10 @@
+import 'package:graphql/client.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'graph_ql_provider.g.dart';
+
+@riverpod
+GraphQLClient graphQlClient(GraphQlClientRef ref) {
+  final httpLink = HttpLink('https://rickandmortyapi.com/graphql');
+  return GraphQLClient(link: httpLink, cache: GraphQLCache());
+}
