1eb3b3bf54043a6ef8daec64e335721cc587ef92
FabianVarela <jfabian.303@gmail.com>
2023-01-07 18:45:27 -0500

    Create test methods to test notifier classes

---
+++ test/features/detail/notifier/rick_morty_detail_notifier_test.dart
@@ -0,0 +1,54 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:rick_morty_app/features/detail/model/rick_morty_detail_model.dart';
+import 'package:rick_morty_app/features/detail/presentation/notifier/rick_morty_detail_notifier.dart';
+import 'package:rick_morty_app/features/detail/repository/rick_morty_detail_repository.dart';
+
+import '../../../factory/helpers.dart';
+import '../../../factory/mocks.dart';
+
+void main() {
+  test('Initial test to build $RickMortyDetailResult', () async {
+    // arrange
+    const detailId = 1;
+
+    final rickMortyDetailRepository = MockDetailRepository();
+    final container = setProviderContainer(
+      overrides: [
+        rickMortyDetailRepoProvider.overrideWithValue(
+          rickMortyDetailRepository,
+        ),
+      ],
+    );
+
+    final listener = Listener<AsyncValue<RickMortyDetailResult>>();
+    when(() => rickMortyDetailRepository.fetchDetailData(id: any(named: 'id')))
+        .thenAnswer((_) async => rickMortyDetailData);
+
+    final notifier = container.read(rickMortyDetailProvider(detailId).notifier);
+
+    // act
+    container.listen(
+      rickMortyDetailProvider(detailId),
+      listener,
+      fireImmediately: true,
+    );
+
+    await notifier.build(detailId);
+
+    // assert
+    verifyInOrder([
+      () => listener(null, const AsyncLoading<RickMortyDetailResult>()),
+      () => listener(
+            const AsyncLoading<RickMortyDetailResult>(),
+            AsyncData<RickMortyDetailResult>(rickMortyDetailData),
+          ),
+    ]);
+
+    verifyNoMoreInteractions(listener);
+    verify(
+      () => rickMortyDetailRepository.fetchDetailData(id: any(named: 'id')),
+    );
+  });
+}
---
+++ test/features/list/notifier/rick_morty_list_notifier_test.dart
@@ -0,0 +1,108 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:rick_morty_app/features/list/model/rick_morty_list_model.dart';
+import 'package:rick_morty_app/features/list/presentation/notifier/rick_morty_list_notifier.dart';
+import 'package:rick_morty_app/features/list/repository/rick_morty_list_repository.dart';
+
+import '../../../factory/helpers.dart';
+import '../../../factory/mocks.dart';
+
+void main() {
+  test('Initial test to build $RickMortyListData', () async {
+    // arrange
+    final rickMortyListRepository = MockListRepository();
+    final container = setProviderContainer(
+      overrides: [
+        rickMortyListRepoProvider.overrideWithValue(rickMortyListRepository),
+      ],
+    );
+
+    final listener = Listener<AsyncValue<RickMortyListData>>();
+    when(rickMortyListRepository.fetchListData).thenAnswer(
+      (_) async => rickMortyListData,
+    );
+    final notifier = container.read(rickMortyListProvider.notifier);
+
+    // act
+    container.listen(rickMortyListProvider, listener, fireImmediately: true);
+    await notifier.build();
+
+    // assert
+    verifyInOrder([
+      () => listener(null, const AsyncLoading<RickMortyListData>()),
+      () => listener(
+            const AsyncLoading<RickMortyListData>(),
+            AsyncData<RickMortyListData>(rickMortyListData),
+          ),
+    ]);
+
+    verifyNoMoreInteractions(listener);
+    verify(rickMortyListRepository.fetchListData);
+  });
+
+  test('Call fetchListData when I change page', () async {
+    // arrange
+    final rickMortyListRepository = MockListRepository();
+    final container = setProviderContainer(
+      overrides: [
+        rickMortyListRepoProvider.overrideWithValue(rickMortyListRepository),
+      ],
+    );
+
+    final listener = Listener<AsyncValue<RickMortyListData>>();
+    when(() => rickMortyListRepository.fetchListData(page: any(named: 'page')))
+        .thenAnswer((_) async => rickMortyListData);
+    final notifier = container.read(rickMortyListProvider.notifier);
+
+    // act
+    container.listen(rickMortyListProvider, listener, fireImmediately: true);
+    await notifier.fetchListData(page: 2);
+
+    // assert
+    final asyncData = AsyncData<RickMortyListData>(rickMortyListData);
+    verifyInOrder([
+      () => listener(null, const AsyncLoading<RickMortyListData>()),
+      () => listener(const AsyncLoading<RickMortyListData>(), asyncData),
+      () => listener(asyncData, asyncData)
+    ]);
+
+    verifyNoMoreInteractions(listener);
+    verify(
+      () => rickMortyListRepository.fetchListData(page: any(named: 'page')),
+    );
+  });
+
+  test('Call fetchListData when I filter data', () async {
+    // arrange
+    final rickMortyListRepository = MockListRepository();
+    final container = setProviderContainer(
+      overrides: [
+        rickMortyListRepoProvider.overrideWithValue(rickMortyListRepository),
+      ],
+    );
+
+    final listener = Listener<AsyncValue<RickMortyListData>>();
+    when(
+      () => rickMortyListRepository.fetchListData(filter: any(named: 'filter')),
+    ).thenAnswer((_) async => rickMortyListData);
+    final notifier = container.read(rickMortyListProvider.notifier);
+
+    // act
+    container.listen(rickMortyListProvider, listener, fireImmediately: true);
+    await notifier.fetchListData(filter: {'name': 'Rick Sanchez'});
+
+    // assert
+    final asyncData = AsyncData<RickMortyListData>(rickMortyListData);
+    verifyInOrder([
+      () => listener(null, const AsyncLoading<RickMortyListData>()),
+      () => listener(const AsyncLoading<RickMortyListData>(), asyncData),
+      () => listener(asyncData, asyncData)
+    ]);
+
+    verifyNoMoreInteractions(listener);
+    verify(
+      () => rickMortyListRepository.fetchListData(filter: any(named: 'filter')),
+    );
+  });
+}
