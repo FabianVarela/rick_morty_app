4c02217063310db8e73cfe6985bbc032a1224c78
FabianVarela <jfabian.303@gmail.com>
2023-01-07 12:54:01 -0500

    Create test to cover graph ql provider

---
+++ test/core/graph_ql/graph_ql_provider_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:graphql/client.dart';
+import 'package:rick_morty_app/core/graph_ql/graph_ql_provider.dart';
+
+import '../../factory/helpers.dart';
+
+void main() {
+  test('Check if graphQl injected is correct', () {
+    // arrange
+    final container = makeProviderContainer();
+    final client = container.read(graphQlClientProvider);
+
+    // assert
+    expect(client.link, isA<HttpLink>());
+    expect(
+      (client.link as HttpLink).uri,
+      Uri.parse('https://rickandmortyapi.com/graphql'),
+    );
+  });
+}
