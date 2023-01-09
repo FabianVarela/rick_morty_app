196d1e7e09030c2d856b7d978dd981d2545f086c
FabianVarela <jfabian.303@gmail.com>
2023-01-09 12:45:52 -0500

    Create fake for Async value class

--- test/factory/mocks.dart
+++ test/factory/mocks.dart
@@ -1,4 +1,5 @@
 import 'package:graphql/client.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:rick_morty_app/features/detail/model/rick_morty_detail_model.dart';
 import 'package:rick_morty_app/features/detail/repository/rick_morty_detail_repository.dart';
@@ -101,3 +102,6 @@ class Listener<T> extends Mock {
 }
 
 class MockOperationException extends Mock implements OperationException {}
+
+// ignore: subtype_of_sealed_class
+class FakeAsyncValue<T> extends Fake implements AsyncValue<T> {}
