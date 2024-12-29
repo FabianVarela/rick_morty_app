dd2b1b95fa574fca43640eeb5aa7bcaa6bad01d0
FabianVarela <jfabian.303@gmail.com>
2024-12-29 17:55:55 -0500

    Use Page correctly

--- lib/core/routes/navigator_routes.dart
+++ lib/core/routes/navigator_routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:rick_morty_app/core/routes/page_routes.dart';
 import 'package:rick_morty_app/features/detail/presentation/view/rick_morty_detail_view.dart';
 import 'package:rick_morty_app/features/list/presentation/view/rick_morty_list_view.dart';
 
@@ -11,15 +12,15 @@ final rickMortyRouter = GoRouter(
   routes: <RouteBase>[
     GoRoute(
       path: '/',
-      pageBuilder: (_, state) => _setDefaultPageRoute<dynamic>(
-        pageKey: state.pageKey,
+      pageBuilder: (_, state) => ScreenPage<dynamic>(
+        key: state.pageKey,
         child: const RickMortyListView(),
       ),
       routes: <GoRoute>[
         GoRoute(
           path: ':id',
-          pageBuilder: (_, state) => _setDefaultPageRoute<dynamic>(
-            pageKey: state.pageKey,
+          pageBuilder: (_, state) => ScreenPage<dynamic>(
+            key: state.pageKey,
             child: RickMortyDetailView(
               id: int.parse(state.pathParameters['id']!),
             ),
@@ -30,14 +31,3 @@ final rickMortyRouter = GoRouter(
   ],
   debugLogDiagnostics: kDebugMode,
 );
-
-Page<dynamic> _setDefaultPageRoute<T>({
-  required LocalKey pageKey,
-  required Widget child,
-}) {
-  final iosPlatforms = [TargetPlatform.iOS, TargetPlatform.macOS];
-  if (iosPlatforms.contains(defaultTargetPlatform)) {
-    return CupertinoPage<T>(key: pageKey, child: child);
-  }
-  return MaterialPage<T>(key: pageKey, child: child);
-}
