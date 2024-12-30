e19c6ec50541fd18ba53ee208b0d5261e6897ab2
FabianVarela <jfabian.303@gmail.com>
2024-12-29 21:06:36 -0500

    Modify test coverage file

--- test_coverage.sh
+++ test_coverage.sh
@@ -1,4 +1,4 @@
-flutter test --coverage
+flutter test --concurrency 4 --coverage --test-randomize-ordering-seed random
 flutter pub run remove_from_coverage -f coverage/lcov.info -r '\.g\.dart$'
 flutter pub run remove_from_coverage -f coverage/lcov.info -r '\.freezed\.dart$'
 genhtml coverage/lcov.info -o coverage/html -q
