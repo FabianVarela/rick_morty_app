1363558037e949ac3a5fc0b6a3e328cc1a41475b
FabianVarela <jfabian.303@gmail.com>
2025-01-06 18:15:41 -0500

    Create asset gen to handle files

---
+++ lib/common/gen/assets.gen.dart
@@ -0,0 +1,37 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsSvgGen {
+  const $AssetsSvgGen();
+
+  /// File path: assets/svg/characters.svg
+  String get characters => 'assets/svg/characters.svg';
+
+  /// File path: assets/svg/episodes.svg
+  String get episodes => 'assets/svg/episodes.svg';
+
+  /// File path: assets/svg/location.svg
+  String get location => 'assets/svg/location.svg';
+
+  /// File path: assets/svg/rick_morty_watermark.svg
+  String get rickMortyWatermark => 'assets/svg/rick_morty_watermark.svg';
+
+  /// File path: assets/svg/search.svg
+  String get search => 'assets/svg/search.svg';
+
+  /// List of all assets
+  List<String> get values =>
+      [characters, episodes, location, rickMortyWatermark, search];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsSvgGen svg = $AssetsSvgGen();
+}
