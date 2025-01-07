08e710486894538e5e7949f51578bbc5cc5771b7
FabianVarela <jfabian.303@gmail.com>
2025-01-06 19:52:01 -0500

    Use StatefulShell instead

--- lib/core/routes/navigator_routes.dart
+++ lib/core/routes/navigator_routes.dart
@@ -1,32 +1,73 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:rick_morty_app/core/routes/page_routes.dart';
+import 'package:rick_morty_app/core/widgets/navigation_scaffold.dart';
 import 'package:rick_morty_app/features/detail/presentation/view/rick_morty_detail_view.dart';
 import 'package:rick_morty_app/features/list/presentation/view/rick_morty_list_view.dart';
 
+final _rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
+final _shellNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'shell');
+
 final rickMortyRouter = GoRouter(
+  initialLocation: '/characters',
   observers: [HeroController()],
-  initialLocation: '/',
+  navigatorKey: _rootNavigatorKey,
   routes: <RouteBase>[
-    GoRoute(
-      path: '/',
-      pageBuilder: (_, state) => ScreenPage<dynamic>(
-        key: state.pageKey,
-        child: const RickMortyListView(),
-      ),
-      routes: <GoRoute>[
-        GoRoute(
-          path: ':id',
-          pageBuilder: (_, state) => ScreenPage<dynamic>(
-            key: state.pageKey,
-            child: RickMortyDetailView(
-              id: int.parse(state.pathParameters['id']!),
+    StatefulShellRoute.indexedStack(
+      branches: <StatefulShellBranch>[
+        StatefulShellBranch(
+          navigatorKey: _shellNavigatorKey,
+          observers: [HeroController()],
+          routes: <RouteBase>[
+            GoRoute(
+              path: '/characters',
+              pageBuilder: (_, state) => ScreenPage<dynamic>(
+                key: state.pageKey,
+                child: const RickMortyListView(),
+              ),
+              routes: <GoRoute>[
+                GoRoute(
+                  path: ':id',
+                  pageBuilder: (_, state) => ScreenPage<dynamic>(
+                    key: state.pageKey,
+                    child: RickMortyDetailView(
+                      id: int.parse(state.pathParameters['id']!),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
+          ],
+        ),
+        StatefulShellBranch(
+          observers: [HeroController()],
+          routes: <RouteBase>[
+            GoRoute(
+              path: '/location',
+              pageBuilder: (_, state) => ScreenPage(
+                key: state.pageKey,
+                child: Scaffold(body: Container()),
+              ),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          observers: [HeroController()],
+          routes: <RouteBase>[
+            GoRoute(
+              path: '/episodes',
+              pageBuilder: (_, state) => ScreenPage(
+                key: state.pageKey,
+                child: Scaffold(body: Container()),
+              ),
+            ),
+          ],
         ),
       ],
+      builder: (_, __, navigationShell) => NavigationScaffold(
+        navigationShell: navigationShell,
+      ),
     ),
   ],
   debugLogDiagnostics: kDebugMode,
