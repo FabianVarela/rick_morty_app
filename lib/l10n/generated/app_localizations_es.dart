674f2a3bcd30b8b720d8fae3da626b9031ad233f
FabianVarela <jfabian.303@gmail.com>
2025-02-17 17:43:30 -0500

    Add l10n generated files

---
+++ lib/l10n/generated/app_localizations_es.dart
@@ -0,0 +1,19 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([String locale = 'es']) : super(locale);
+
+  @override
+  String get characterNavBarTitle => 'Personajes';
+
+  @override
+  String get locationNavBarTitle => 'Lugares';
+
+  @override
+  String get episodeNavBarTitle => 'Episodios';
+}
