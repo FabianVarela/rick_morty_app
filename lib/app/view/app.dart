e790f4ecbde88042f8056d8b0c6db98cd6f03f6c
FabianVarela <jfabian.303@gmail.com>
2025-02-17 18:47:36 -0500

    Modify import and export paths in l10 file

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rick_morty_app/core/routes/navigator_routes.dart';
-import 'package:rick_morty_app/l10n/generated/app_localizations.dart'
-    show AppLocalizations;
+import 'package:rick_morty_app/l10n/l10n.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
-import 'package:rick_morty_app/l10n/generated/app_localizations.dart'
-    show AppLocalizations;
+import 'package:rick_morty_app/l10n/generated/app_localizations.dart';
+
+export 'package:rick_morty_app/l10n/generated/app_localizations.dart';
 
 extension AppLocalizationsX on BuildContext {
   AppLocalizations get l10n => AppLocalizations.of(this);
