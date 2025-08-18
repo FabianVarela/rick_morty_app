725081edd83f6cc9033f54fef3c0413fdb7285cd
FabianVarela <jfabian.303@gmail.com>
2025-08-17 19:01:24 -0500

    Fix deprecated issues

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -9,7 +9,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       theme: ThemeData(
-        appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
+        appBarTheme: const AppBarTheme(backgroundColor: Color(0xFF13B9FF)),
         colorScheme: ColorScheme.fromSwatch(
           accentColor: const Color(0xFF13B9FF),
         ),
