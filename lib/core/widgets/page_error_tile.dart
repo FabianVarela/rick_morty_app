2c24997166607ba3d58fab128200bdcb23ee4c03
FabianVarela <jfabian.303@gmail.com>
2025-12-19 20:15:47 -0500

    Add design when card or tile is a grid

--- lib/core/widgets/page_error_tile.dart
+++ lib/core/widgets/page_error_tile.dart
@@ -1,26 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:rick_morty_app/core/theme/app_theme.dart';
 
 class PageErrorTile extends StatelessWidget {
-  const PageErrorTile({required this.page, required this.onRetry, super.key});
+  const PageErrorTile({
+    required this.page,
+    required this.onRetry,
+    this.isGrid = false,
+    super.key,
+  });
 
   final int page;
   final VoidCallback onRetry;
+  final bool isGrid;
 
   @override
   Widget build(BuildContext context) {
+    final retryButon = ElevatedButton(
+      onPressed: onRetry,
+      child: const Text('Retry'),
+    );
+
     return Padding(
       padding: const .all(16),
-      child: Row(
-        spacing: 8,
-        mainAxisAlignment: .spaceBetween,
-        children: <Widget>[
-          Text('Error loading page: $page'),
-          ElevatedButton(
-            onPressed: onRetry,
-            child: const Text('Retry'),
-          ),
-        ],
-      ),
+      child: switch (isGrid) {
+        true => Column(
+          spacing: 12,
+          mainAxisAlignment: .center,
+          children: <Widget>[
+            Text(
+              'Error loading\npage $page',
+              textAlign: .center,
+              style: context.secondarySubtitle.copyWith(
+                color: context.colors.textSecondary,
+              ),
+            ),
+            retryButon,
+          ],
+        ),
+        false => Row(
+          spacing: 8,
+          mainAxisAlignment: .spaceBetween,
+          children: <Widget>[
+            Text(
+              'Error loading page: $page',
+              style: context.secondarySubtitle.copyWith(
+                color: context.colors.textSecondary,
+              ),
+            ),
+            retryButon,
+          ],
+        ),
+      },
     );
   }
 }
--- lib/features/character_list/presentation/widgets/character_card_shimmer.dart
+++ lib/features/character_list/presentation/widgets/character_card_shimmer.dart
@@ -3,7 +3,9 @@ import 'package:rick_morty_app/core/theme/app_theme.dart';
 import 'package:rick_morty_app/core/widgets/shimmer_widget.dart';
 
 class CharacterCardShimmer extends StatelessWidget {
-  const CharacterCardShimmer({super.key});
+  const CharacterCardShimmer({this.isGrid = false, super.key});
+
+  final bool isGrid;
 
   @override
   Widget build(BuildContext context) {
@@ -12,20 +14,25 @@ class CharacterCardShimmer extends StatelessWidget {
       color: context.colors.backgroundCard,
       clipBehavior: .antiAliasWithSaveLayer,
       shape: RoundedRectangleBorder(borderRadius: .circular(16)),
-      child: const Row(
-        crossAxisAlignment: .start,
-        children: <Widget>[
-          ShimmerBox(borderRadius: .zero, size: .square(140)),
-          Expanded(
-            child: Padding(
-              padding: .all(16),
+      child: switch (isGrid) {
+        true => const Column(
+          crossAxisAlignment: .stretch,
+          children: <Widget>[
+            Expanded(
+              child: AspectRatio(
+                aspectRatio: 1,
+                child: ShimmerBox(borderRadius: .zero),
+              ),
+            ),
+            Padding(
+              padding: .all(12),
               child: Column(
-                spacing: 10,
+                spacing: 6,
                 mainAxisSize: .min,
                 crossAxisAlignment: .start,
                 children: <Widget>[
                   ShimmerBox(
-                    size: Size(150, 18),
+                    size: .fromHeight(18),
                     borderRadius: .all(.circular(4)),
                   ),
                   Row(
@@ -36,7 +43,7 @@ class CharacterCardShimmer extends StatelessWidget {
                         borderRadius: .all(.circular(4)),
                       ),
                       ShimmerBox(
-                        size: Size(120, 14),
+                        size: Size(100, 14),
                         borderRadius: .all(.circular(4)),
                       ),
                     ],
@@ -46,11 +53,11 @@ class CharacterCardShimmer extends StatelessWidget {
                     crossAxisAlignment: .start,
                     children: <Widget>[
                       ShimmerBox(
-                        size: Size(90, 12),
+                        size: Size(60, 10),
                         borderRadius: .all(.circular(4)),
                       ),
                       ShimmerBox(
-                        size: Size(140, 14),
+                        size: Size(120, 12),
                         borderRadius: .all(.circular(4)),
                       ),
                     ],
@@ -58,9 +65,58 @@ class CharacterCardShimmer extends StatelessWidget {
                 ],
               ),
             ),
-          ),
-        ],
-      ),
+          ],
+        ),
+        false => const Row(
+          crossAxisAlignment: .start,
+          children: <Widget>[
+            ShimmerBox(borderRadius: .zero, size: .square(140)),
+            Expanded(
+              child: Padding(
+                padding: .all(16),
+                child: Column(
+                  spacing: 10,
+                  mainAxisSize: .min,
+                  crossAxisAlignment: .start,
+                  children: <Widget>[
+                    ShimmerBox(
+                      size: Size(150, 18),
+                      borderRadius: .all(.circular(4)),
+                    ),
+                    Row(
+                      spacing: 6,
+                      children: <Widget>[
+                        ShimmerBox(
+                          size: .square(8),
+                          borderRadius: .all(.circular(4)),
+                        ),
+                        ShimmerBox(
+                          size: Size(120, 14),
+                          borderRadius: .all(.circular(4)),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      spacing: 4,
+                      crossAxisAlignment: .start,
+                      children: <Widget>[
+                        ShimmerBox(
+                          size: Size(90, 12),
+                          borderRadius: .all(.circular(4)),
+                        ),
+                        ShimmerBox(
+                          size: Size(140, 14),
+                          borderRadius: .all(.circular(4)),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      },
     );
   }
 }
